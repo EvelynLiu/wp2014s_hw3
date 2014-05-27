c48eaf62624d4b06dfbb1c875b41dbc2a0ff3b2a
EvelynLiu <100703007@nccu.edu.tw>
2014-05-28 01:53:52 +0800

    hw3 to gh-pages

--- js/app.js
+++ js/app.js
@@ -37,7 +37,131 @@
 				window.location.hash="login/"
 			})
 		},
-	}
+		evaluationView:t.loginRequiredView(function(){
+			var t=Parse.Object.extend("Evaluation");
+			var n=Parse.User.current();
+			var r=new Parse.ACL;
+			r.setPublicReadAccess(false);
+			r.setPublicWriteAccess(false);
+			r.setReadAccess(n,true);
+			r.setWriteAccess(n,true);
+			var i=new Parse.Query(t);
+			i.equalTo("user",n);
+			i.first({
+				success:function(i){
+					window.EVAL=i;
+					if(i===undefined){
+						var s=TAHelp.getMemberlistOf(n.get("username")).filter(function(e){
+							return e.StudentId!==n.get("username")?true:false
+						}).map(function(e){
+							e.scores=["0","0","0","0"];
+							return e
+						})
+					}
+					else{
+						var s=i.toJSON().evaluations
+					}
+					document.getElementById("content").innerHTML=e.evaluationView(s);
+					document.getElementById("evaluationForm-submit").value=i===undefined?"�銵典":"靽格銵典";
+					document.getElementById("evaluationForm").addEventListener("submit",function(){
+						for(var o=0;o<s.length;o++){
+							for(var u=0;u<s[o].scores.length;u++){
+								var a=document.getElementById("stu"+s[o].StudentId+"-q"+u);
+								var f=a.options[a.selectedIndex].value;s[o].scores[u]=f
+							}
+						}
+						if(i===undefined){
+							i=new t;
+							i.set("user",n);
+							i.setACL(r)}console.log(s);
+							i.set("evaluations",s);
+							i.save(null,{
+								success:function(){
+									document.getElementById("content").innerHTML=e.updateSuccessView()
+								},
+								error:function(){
+
+								}
+							})
+					},false);
+				},
+				error:function(e,t){
+
+				}
+			})
+		}),
+		loginView:function(t){
+	var r=function(e){
+		var t=document.getElementById(e).value;
+		return TAHelp.getMemberlistOf(t)===false?false:true
+	};
+	var i=function(e,t,n){
+		if(!t()){
+			document.getElementById(e).innerHTML=n;
+			document.getElementById(e).style.display="block"
+		}
+		else{
+			document.getElementById(e).style.display="none"
+		}
+	};
+	var s=function(){
+		n.navbar();
+		window.location.hash=t?t:""
+	};
+	var o=function(){
+		var e=document.getElementById("form-signup-password");
+		var t=document.getElementById("form-signup-password1");
+		var n=e.value===t.value?true:false;
+		i("form-signup-message",function(){return n},"Passwords don't match.");
+		return n
+	};
+	document.getElementById("content").innerHTML=e.loginView();
+	document.getElementById("form-signin-student-id").addEventListener("keyup",function(){
+		i("form-signin-message",function(){
+			return r("form-signin-student-id")
+		},"The student is not one of the class students.")
+	});
+	document.getElementById("form-signin").addEventListener("submit",function(){
+		if(!r("form-signin-student-id")){
+			alert("The student is not one of the class students.");
+			return false
+		}
+		Parse.User.logIn(document.getElementById("form-signin-student-id").value,document.getElementById("form-signin-password").value,{
+			success:function(e){
+				s()
+			},
+			error:function(e,t){
+				i("form-signin-message",function(){return false},"Invaild username or password.")
+			}
+		})
+	},false);
+	document.getElementById("form-signup-student-id").addEventListener("keyup",function(){
+		i("form-signup-message",function(){return r("form-signup-student-id")},"The student is not one of the class students.")
+	});
+	document.getElementById("form-signup-password1").addEventListener("keyup",o);
+	document.getElementById("form-signup").addEventListener("submit",function(){
+		if(!r("form-signup-student-id")){
+			alert("The student is not one of the class students.");
+			return false
+		}
+		var e=o();
+		if(!e){
+			return false
+		}
+		var t=new Parse.User;
+		t.set("username",document.getElementById("form-signup-student-id").value);
+		t.set("password",document.getElementById("form-signup-password").value);
+		t.set("email",document.getElementById("form-signup-email").value);
+		t.signUp(null,{
+			success:function(e){
+				s()
+			},
+			error:function(e,t){
+				i("form-signup-message",function(){return false},t.message)
+			}
+		})
+	},false)
+	}};
 	var r=Parse.Router.extend({
 		routes:{
 			"":"indexView",
@@ -52,7 +176,7 @@
 	Parse.history.start();
 	n.navbar()
 })
-()*/
+()
 /*(function(){
   Parse.initialize("DW5mOdnS4vTuOSb2did1Rb2HDulVVjPOCZZE6eGK","PvS7AF0t6hk04XSyhhUWoHDJsIQrlmNZZwFS4xOw");//初始化Parse()
   
