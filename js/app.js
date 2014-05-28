51a27bc7ab805b3271ff6c5e3e15dc4174e4614b
EvelynLiu <100703007@nccu.edu.tw>
2014-05-28 13:10:11 +0800

    hw3 to gh-pages

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
 (function(){
+	// 初始化Parse
 	Parse.initialize("DW5mOdnS4vTuOSb2did1Rb2HDulVVjPOCZZE6eGK","PvS7AF0t6hk04XSyhhUWoHDJsIQrlmNZZwFS4xOw");
+	//編譯template engine函數();
 	var e={}; // e--->template
 	["loginView","evaluationView","updateSuccessView"].forEach(function(t){
 		templateCode=document.getElementById(t).text;
 		e[t]=doT.template(templateCode)
 	});
+	
 	var t={
 		loginRequiredView:function(e){
 			return function(){
@@ -38,6 +41,7 @@
 			})
 		},
 		evaluationView:t.loginRequiredView(function(){
+		// 基本上和上課範例購物車的函數很相似，這邊會用Parse DB	
 			var t=Parse.Object.extend("Evaluation");
 			var n=Parse.User.current();
 			var r=new Parse.ACL;
@@ -50,6 +54,8 @@
 			i.first({
 				success:function(i){
 					window.EVAL=i;
+					// 問看看Parse有沒有這個使用者之前提交過的peer review物件
+      				// 沒有的話: 從TAHelp生一個出來(加上scores: [‘0’, ‘0’, ‘0’, ‘0’]屬性存分數並把自己排除掉
 					if(i===undefined){
 						var s=TAHelp.getMemberlistOf(n.get("username")).filter(function(e){
 							return e.StudentId!==n.get("username")?true:false
@@ -61,8 +67,11 @@
 					else{
 						var s=i.toJSON().evaluations
 					}
+					// 把peer review物件裡的東西透過版型印到瀏覽器上
 					document.getElementById("content").innerHTML=e.evaluationView(s);
+					// 登入時檢查是否已評分，若已評分將「送出表單」改變為「修改表單」
 					document.getElementById("evaluationForm-submit").value=i===undefined?"Submit the form":"Modify the form";
+					// 綁定表單送出的事件
 					document.getElementById("evaluationForm").addEventListener("submit",function(){
 						for(var o=0;o<s.length;o++){
 							for(var u=0;u<s[o].scores.length;u++){
@@ -70,6 +79,7 @@
 								var f=a.options[a.selectedIndex].value;s[o].scores[u]=f
 							}
 						}
+						//如果Parse沒有之前提交過的peer review物件，要自己new一個。或更新分數然後儲存。
 						if(i===undefined){
 							i=new t;
 							i.set("user",n);
@@ -90,12 +100,12 @@
 				}
 			})
 		}),
-		loginView:function(t){
+		loginView:function(t){ // check id
 	var r=function(e){
 		var t=document.getElementById(e).value;
 		return TAHelp.getMemberlistOf(t)===false?false:true
 	};
-	var i=function(e,t,n){
+	var i=function(e,t,n){ // display
 		if(!t()){
 			document.getElementById(e).innerHTML=n;
 			document.getElementById(e).style.display="block"
@@ -104,28 +114,33 @@
 			document.getElementById(e).style.display="none"
 		}
 	};
-	var s=function(){
+	var s=function(){  // success view
 		n.navbar();
 		window.location.hash=t?t:""
 	};
-	var o=function(){
+	var o=function(){ // check password
 		var e=document.getElementById("form-signup-password");
 		var t=document.getElementById("form-signup-password1");
 		var n=e.value===t.value?true:false;
 		i("form-signup-message",function(){return n},"Passwords don't match.");
 		return n
 	};
+	// 把版型印到瀏覽器上
 	document.getElementById("content").innerHTML=e.loginView();
+	// 登入
+	// 綁定登入表單的學號檢查事件 (打字時)
 	document.getElementById("form-signin-student-id").addEventListener("keyup",function(){
 		i("form-signin-message",function(){
 			return r("form-signin-student-id")
 		},"The student is not one of the class students.")
 	});
 	document.getElementById("form-signin").addEventListener("submit",function(){
+		// 綁定登入表單的學號檢查事件
 		if(!r("form-signin-student-id")){
 			alert("The student is not one of the class students.");
 			return false
 		}
+		// 綁定登入表單的登入檢查事件
 		Parse.User.logIn(document.getElementById("form-signin-student-id").value,document.getElementById("form-signin-password").value,{
 			success:function(e){
 				s()
@@ -135,19 +150,23 @@
 			}
 		})
 	},false);
+	// 註冊
 	document.getElementById("form-signup-student-id").addEventListener("keyup",function(){
 		i("form-signup-message",function(){return r("form-signup-student-id")},"The student is not one of the class students.")
 	});
 	document.getElementById("form-signup-password1").addEventListener("keyup",o);
 	document.getElementById("form-signup").addEventListener("submit",function(){
+		// 綁定註冊表單的學號檢查事件
 		if(!r("form-signup-student-id")){
 			alert("The student is not one of the class students.");
 			return false
 		}
+		// 綁定註冊表單的密碼檢查事件
 		var e=o();
 		if(!e){
 			return false
 		}
+		// 綁定註冊表單的註冊檢查事件
 		var t=new Parse.User;
 		t.set("username",document.getElementById("form-signup-student-id").value);
 		t.set("password",document.getElementById("form-signup-password").value);
@@ -172,12 +191,14 @@
 		evaluationView:n.evaluationView,
 		loginView:n.loginView
 	});
+	//讓router活起來
 	this.Router=new r;
 	Parse.history.start();
 	n.navbar()
 })
 ()
-/*(function(){
+/*
+(function(){
   Parse.initialize("DW5mOdnS4vTuOSb2did1Rb2HDulVVjPOCZZE6eGK","PvS7AF0t6hk04XSyhhUWoHDJsIQrlmNZZwFS4xOw");//初始化Parse()
   
   var templates = {};
@@ -245,10 +266,10 @@
 		};
     	// 把版型印到瀏覽器上
     	document.getElementById('content').innerHTML = templates.loginView();
+    	// 登入
     	document.getElementById("form-signin-student-id").addEventListener("keyup",function(){
     		i("form-signin-message",function(){return r("form-signin-student-id")},"The student is not one of the class students.")
     	});
-    	// 登入
     	document.getElementById("form-signin").addEventListener("submit",function(){
 			// 綁定登入表單的學號檢查事件
 			if(!r("form-signin-student-id")){
